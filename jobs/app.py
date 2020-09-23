e8de958ee75764c3f80a8061d978d645ad402cda
asc-it-minds <asc@it-minds.dk>
2020-09-23 13:25:09 +0200

    M3

--- jobs/app.py
+++ jobs/app.py
@@ -1,7 +1,18 @@
 from flask import Flask
-from flask import render_template
+from flask import render_template, helpers
+from flask.globals import g
+import sqlite3
+
+PATH='db/jobs.sqlite'
 
 app = Flask(__name__)
+
+def open_connection():
+    connection = getattr(g, '_connection', None)
+    if (connection == None):
+        connection = sqlite3.connect(PATH)
+        g._connection = sqlite3.connect(PATH)
+
 
 @app.route('/')
 @app.route('/jobs')
